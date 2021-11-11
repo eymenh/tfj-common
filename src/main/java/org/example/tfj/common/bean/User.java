3fe32557647af2b84816820ad3c612d542709f59
unknown <hatoum.eymen@gmail.com>
2021-11-11 20:47:35 +0100

    Initial commit

---
+++ src/main/java/org/example/tfj/common/bean/User.java
@@ -0,0 +1,31 @@
+package org.example.tfj.common.bean;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private long id;
+
+    @Getter
+    @Setter
+    private String lastName;
+
+    @Getter
+    @Setter
+    private String firstName;
+
+    @Getter
+    @Setter
+    private String email;
+}
